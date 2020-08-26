e1d10e5403079bb0fac391e03f1b1f455fac8783
AbdullahMouzeen <abdullah.mouazeen@gmail.com>
2020-08-26 23:57:06 +0300

    loop

--- java.js
+++ java.js
@@ -1,10 +1,15 @@
 var old = prompt('how old are you');
 var status;
 
+while (old =='' || old ==null || isNaN(old) || old >66 ) {
+old = prompt('YOU ARE SO ANCIENT ===> HOW OLD ARE YOU');
+
+}
+
 function ageness(num){
-    var  age 
-    age = old *1.1
-    return age
+    var  paymint ;
+    paimint = old *1.1;
+    return paimint;
     }
 
 if ( old > 18 && 55 > old) {
@@ -18,6 +23,8 @@ status = 'YOU DONT HAVE TO BE HERE !';
 else if (old >=55) {
 status = 'YOU ABOUT TO DIY! ' ;}
 
- var paymint; 
+  
 
 document.write('<h3>' + status + '  ======> ========> =======> '+' your monthly paymint is : '  +  ageness(status) + ' $  for VIP club ' + '<h3>');
+
+
